83c4f51cfe4c1ecea4970b13f97d63c69bda53c1
nycolemendonca-acs <135253384+nycolemendonca-acs@users.noreply.github.com>
2023-06-02 17:08:14 -0300

    Add files via upload

---
+++ product-api/src/main/resources/db/migration/v3__insert_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO products.category(id, nome) VALUES(1, 'Eletrônicos');
+INSERT INTO products.category(id, nome) VALUES(2, 'Móveis');
+INSERT INTO products.category(id, nome) VALUES(3, 'Brinquedos');
