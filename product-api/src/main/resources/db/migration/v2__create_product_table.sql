185046a1812c7da3036b6054855fec94b8446922
nycolemendonca-acs <135253384+nycolemendonca-acs@users.noreply.github.com>
2023-06-05 10:43:02 -0300

    Delete product-api directory

--- product-api/src/main/resources/db/migration/v2__create_product_table.sql
+++
@@ -1,8 +0,0 @@
-create table products.product (
-    id bigserial primary key,
-    product_identifier varchar not null,
-    nome varchar(100) not null,
-    descricao varchar not null,
-    preco float not null,
-    category_id biginit REFERENCES products.category(id)
-);
--- product-api/src/main/resources/db/migration/v3__insert_categories.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO products.category(id, nome) VALUES(1, 'Eletrônicos');
-INSERT INTO products.category(id, nome) VALUES(2, 'Móveis');
-INSERT INTO products.category(id, nome) VALUES(3, 'Brinquedos');
